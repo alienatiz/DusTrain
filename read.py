beba6d67cab8947c5e7c42febc2d090d61491ca8
Byeongcheol Kim <kbc20000@gmail.com>
2022-10-14 15:31:31 +0900

    DusTrain: Fix the right names of columns
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- read.py
+++ read.py
@@ -7,26 +7,27 @@ input_path = r'C:/Users/KBC/PycharmProjects/chemical/outputs/'
 pre_path = r'C:/Users/KBC/PycharmProjects/chemical/outputs/'
 
 # 1. Read data from csv
-raw_data = pd.read_csv(input_path + 'gwangyang.csv')
-print(raw_data, '\n')
+raw_data = pd.read_csv(input_path + 'data_c.csv')
+print("raw_data.shape: \n", raw_data.shape)
+print("raw_data.describe: \n", raw_data.describe())
 
 # 2. Check the null values
 # print(data.dtypes, '\n', data_g.dtypes)
-print('data.isnull().sum(): \n', raw_data.isnull().sum())
-print('raw_data.isna().sum(): \n', raw_data.isna().sum())
-raw_data.replace(['보수중', '기기점검', '측정자료확인중(가동중지)', '측정자료확인중'], np.NAN, inplace=True)
-print(raw_data, '\n')
+# print('data.isnull().sum(): \n', raw_data.isnull().sum())
+# print('raw_data.isna().sum(): \n', raw_data.isna().sum())
 
 # 3. Pre-process the null values
-pre_data = raw_data.drop(['암모니아_허용기준', '암모니아_측정값', '질소산화물_허용기준', '황산화물_허용기준',
-                          '먼지_허용기준', '불화수소_허용기준', '불화수소_측정값', '염화수소_허용기준', '일산화탄소_허용기준'], axis=1)
-pre_data = pre_data.rename(columns={'측정시간': 'measure_dt', '지역명': 'area_nm', '사업장명': 'fact_nm',
-                                    '배출구': 'stack', '질소산화물_측정값': 'NOx', '황산화물_측정값': 'SOx',
-                                    '먼지_측정값': 'DUST', '염화수소_측정값': 'HCl', '일산화탄소_측정값': 'CO'})
-print('columns of pre_data: ', pre_data.columns)
+pre_data = raw_data.drop(['nh3_exhst_perm_stdr_value', 'nh3_mesure_value', 'nox_exhst_perm_stdr_value',
+                          'sox_exhst_perm_stdr_value', 'tsp_exhst_perm_stdr_value', 'hf_exhst_perm_stdr_value',
+                          'hf_mesure_value', 'hcl_exhst_perm_stdr_value', 'co_exhst_perm_stdr_value'], axis=1)
+pre_data.rename(columns={'nox_mesure_value': 'NOx', 'sox_mesure_value': 'SOx',
+                         'tsp_mesure_value': 'DUST', 'hcl_mesure_value': 'HCl', 'co_mesure_value': 'CO'}, inplace=True)
+
+# print('columns of pre_data: ', pre_data.columns)
+print("pre_data.shape: \n", pre_data.shape)
+print("pre_data.describe: \n", pre_data.describe())
 pre_data.to_csv(pre_path + 'pre_data.csv', index=False)
 pre_data_loaded = pd.read_csv(pre_path + 'pre_data.csv')
 
 # 4. Re-check the null values
-# print(data_p.dtypes, '\n', data_g.dtypes)
 print('pre_data_loaded.isnull().sum(): \n', pre_data_loaded.isnull().sum())
