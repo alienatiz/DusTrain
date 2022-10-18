c614f8fc49b6ea70fe40e702ef21b6164ce76205
Byeongcheol Kim <kbc20000@gmail.com>
2022-10-18 16:09:03 +0900

    DusTrain: Drop the unnecessary columns
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- train.py
+++ train.py
@@ -1,18 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import sklearn
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 
 # 1. Load data
 datasets_path = r'C:/Users/KBC/PycharmProjects/chemical/datasets/'
-data_raw = pd.read_csv(datasets_path + 'data_r2.csv')
-
-print(data_raw.shape)
+data_fix = pd.read_csv(datasets_path + 'data_r2.csv')
+data_fix.drop(['mesure_dt', 'area_nm', 'fact_manage_nm', 'stack_code'], axis=1, inplace=True)
+data_fix.to_csv(datasets_path + 'data_r2_fix.csv', sep=',', index=False, encoding='UTF-8-sig')
 # 7: 2333
 # 3: 1001
 
 # 2. Split the data to train, test
-X = data_raw.loc[:2331]
-y = data_raw.loc[2332:3334]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
+data_raw = pd.read_csv(datasets_path + 'data_r2_fix.csv')
+print(data_raw.shape, data_raw.dtypes)
