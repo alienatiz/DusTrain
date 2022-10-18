3f3932ebe4315358fe853ed5e4233227603f3a9a
Byeongcheol Kim <kbc20000@gmail.com>
2022-10-18 16:12:26 +0900

    DusTrain: Optimize the library import
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- train.py
+++ train.py
@@ -1,20 +1,24 @@
-import numpy as np
+import platform
+
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
-import sklearn
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
+from matplotlib import font_manager, rc
+
+plt.rcParams['axes.unicode_minus'] = False
+
+if platform.system() == 'Darwin':
+    rc('font', family='AppleGothic')
+elif platform.system() == 'Windows':
+    path = "c:/Windows/Fonts/malgun.ttf"
+    font_name = font_manager.FontProperties(fname=path).get_name()
+    rc('font', family=font_name)
+else:
+    print('Unknown System... ')
 
 # 1. Load data
 datasets_path = r'C:/Users/KBC/PycharmProjects/chemical/datasets/'
 data_fix = pd.read_csv(datasets_path + 'data_r2.csv')
 data_fix.drop(['mesure_dt', 'area_nm', 'fact_manage_nm', 'stack_code'], axis=1, inplace=True)
 data_fix.to_csv(datasets_path + 'data_r2_fix.csv', sep=',', index=False, encoding='UTF-8-sig')
-# 7: 2333
-# 3: 1001
-
-# 2. Split the data to train, test
 data_raw = pd.read_csv(datasets_path + 'data_r2_fix.csv')
 print(data_raw.shape, data_raw.dtypes)
