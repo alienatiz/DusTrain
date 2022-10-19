d7253158cd44afc2b1627e8c84f7fefdb2d2af43
Byeongcheol Kim <kbc20000@gmail.com>
2022-10-19 15:50:50 +0900

    DusTrain: Add the LinearRegression, SVR Models
    * They are not completed methods yet.
    
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- train.py
+++ train.py
@@ -5,7 +5,9 @@ import pandas as pd
 import sklearn
 from matplotlib import font_manager, rc
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
 
 plt.rcParams['axes.unicode_minus'] = False
 
@@ -27,18 +29,32 @@ data_raw = pd.read_csv(datasets_path + 'data_r2_fix.csv')
 print(data_raw.shape, data_raw.dtypes)
 
 # 2. Split the data to train, test
-X = data_raw.iloc[:, 0:2]
+X = data_raw.iloc[:, 0:3]
 y = data_raw.iloc[:, 4:]
 
 features = ['NOx', 'SOx', 'DUST', 'HCl', 'CO']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# Build RF model
+# Build LR model
 print("\n*** Build LinearRegression model ***")
 print(f'Checked sklearn version: {sklearn.__version__}')
-treeModel_RF = RandomForestRegressor(n_estimators=500, n_jobs=8, oob_score=True, random_state=0)
-treeModel_RF.fit(X_train, y_train.values.ravel())
-importance = treeModel_RF.feature_importances_
+LR = LinearRegression()
+LR.fit(X_train, y_train)
+print('기울기 a:', LR.coef_)
+print('y절편 b:', LR.intercept_)
+relation_square = LR.score(X_train, y_train)
+print('결정 계수 r', relation_square)
+
+# 기울기 a: [[-0.00225756  0.02130299 -0.05156007 -0.00456313]]
+# y절편 b: [0.27532916]
+# 결정 계수 r 0.004672870461478795
+
+# Build RF model
+print("\n*** Build RandomForestRegressor model ***")
+print(f'Checked sklearn version: {sklearn.__version__}')
+RFR = RandomForestRegressor(n_estimators=500, n_jobs=8, oob_score=True, random_state=0)
+RFR.fit(X_train, y_train.values.ravel())
+importance = RFR.feature_importances_
 print(importance)
 
 # NOX: 0.31325047
@@ -52,3 +68,14 @@ print(importance)
 
 # NOX: 0.69537026
 # SOX: 0.30462974
+
+# Build SVR model
+print("\n*** Build SupportVectorRegressor model ***")
+print(f'Checked sklearn version: {sklearn.__version__}')
+SV = SVR()
+SV.fit(X_train, y_train.values.ravel())
+print('결정 계수 R:', SV.score(X_train, y_train))
+# 결정 계수 R: 0.00657197919417607
+y_pred = SV.predict(X_train)
+print(y_pred)
+# [0.16443919 0.15018678 0.10979136 ... 0.124581   0.12575119 0.16421619]
