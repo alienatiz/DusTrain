31623df25bb47864377ccaf82ad06aba75070186
Byeongcheol Kim <kbc20000@gmail.com>
2022-10-31 20:09:32 +0900

    DusTrain: Transpose the rows and columns from datasets
    * This is the dataset which is time-series.
    
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

---
+++ transform.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+data_path = r'C:\\Users\KBC\PycharmProjects\chemical\datasets'
+datasets = pd.read_csv('C:\\Users\KBC\PycharmProjects\chemical\datasets\data_r2_fix.csv')
+print(datasets.head(5))
+
+fixed_datasets = datasets.transpose()
+print(fixed_datasets.head(5))
+
+fixed_datasets.to_csv(data_path + '\data_r2_fix_tp.csv', sep=',', na_rep='NaN', index=False, encoding='UTF-8-sig')
