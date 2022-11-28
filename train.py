df1fe5e5f96f0764e969729ce35cd442ee96d2a6
Byeongcheol Kim <kbc20000@gmail.com>
2022-11-28 19:01:53 +0900

    DusTrain: Improve the structure of codes
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- train.py
+++ train.py
@@ -40,6 +40,8 @@ target_path = r'C:/Users/KBC/PycharmProjects/chemical/datasets/target/'
 
 data_raw = pd.read_csv(datasets_path + 'datasets_08_10.csv')
 data_target = pd.read_csv(datasets_path + 'datasets_09.csv')
+data_fix = pd.read_csv(datasets_path + 'datasets_0801-1017.csv')
+print(data_fix.head())
 
 data_raw.dropna(inplace=True)
 data_target.dropna(inplace=True)
@@ -50,25 +52,70 @@ def transform_datetype(data_raw):
     data_raw['mesure_dt'] = pd.to_datetime(data_raw['mesure_dt'])
     data_raw.set_index(data_raw['mesure_dt'], inplace=True)
     data_raw.drop(['mesure_dt'], axis=1, inplace=True)
-    data_raw.sort_values(by = ['mesure_dt'])
+    data_raw.sort_values(by=['mesure_dt'])
+    data_raw.dropna()
+    return data_raw
+
+
+def dt2float(data_raw):
+    data_raw['mesure_dt'] = data_raw.index.astype('float[D]').tolist()
     return data_raw
 
 
 data_raw = transform_datetype(data_raw)
-print(data_raw.shape)
-print(data_target.shape)
+data_fix = transform_datetype(data_fix)
+data_rnd = data_fix
+print('data_raw.shape:\n', data_raw.shape)
+print('data_target.shape:\n', data_target.shape)
+print('data_fix.shape:\n', data_fix.shape)
 
 # 2. Split the data to train, test from datasets
 # Description: The data collected from August to October is set to train dataset,
 #              and the data collected from the certain week of October is set to validation dataset.
-features = ['NOx', 'SOx', 'HCl', 'CO']
-print(data_raw)
-data_raw_train = data_raw.iloc[1:2741, :]
-data_raw_test = np.log(data_raw.iloc[2742:3141, :])
-print(data_raw_train, data_raw_test)
-print(data_raw_train.shape, data_raw_test.shape)
+# X_train = data_raw_train[['NOx', 'SOx', 'HCl', 'CO']]
+# X_test = data_raw_test[['NOx', 'SOx', 'HCl', 'CO']]
 
-# 2-1. Visualizing the datasets
+# 2-1. Time-Series
+
+features = ['NOx', 'SOx', 'HCl', 'CO']
+print('\nSplit the datasets based on Time-series')
+X_tr_ts = data_fix.iloc[1:2740, 0:2]
+X_te_ts = data_fix.iloc[2741:3140, 0:2]
+print(X_te_ts)
+print(X_tr_ts, X_te_ts)
+print(X_tr_ts.shape, X_te_ts.shape)
+
+X_train_ts = X_tr_ts.sort_index().iloc[:, :]
+y_train_ts = X_tr_ts[['NOx', 'SOx']]
+X_test_ts = X_te_ts.sort_index().iloc[:, :]
+y_test_ts = X_te_ts[['NOx', 'SOx']]
+
+print('\nCheck the size of datasets_rnd:',
+      '\nX_train_ts: ', X_train_ts.shape,
+      '\ny_train_ts: ', y_train_ts.shape,
+      '\nX_test_ts: ', X_test_ts.shape,
+      '\ny_test_ts: ', y_test_ts.shape)
+
+# 2-2. Random
+corr = data_rnd.corr(method='pearson', numeric_only=True)
+print('corr:\n', corr)
+
+print(data_rnd.index.dtype)
+dt2float(data_rnd)
+X = data_rnd.index.values.reshape(-1,1)
+y = data_rnd[['NOx', 'SOx']].values
+print('\nSplit the datasets randomly')
+X_train_rnd, X_test_rnd, y_train_rnd, y_test_rnd = train_test_split(X, y, test_size=0.2, random_state=0)
+print('\nCheck the size of datasets_rnd:',
+      '\nX_train_rnd: ', X_train_rnd.shape,
+      '\ny_train_rnd: ', y_train_rnd.shape,
+      '\nX_test_rnd: ', X_test_rnd.shape,
+      '\ny_test_rnd: ', y_test_rnd.shape)
+y_train_rnd.reshape(-1, 1)
+y_test_rnd.reshape(-1, 1)
+
+
+# 2-3. Visualizing the datasets
 sns.set_style("ticks")
 sns.set_context("paper")
 sns.set(font_scale=1.5)
@@ -80,38 +127,40 @@ sns1 = plt
 plt.rc('font', family=font_name)
 plt.style.use('default')
 sns1.figure(figsize=(20,10))
-sns1.plot(data_raw.index, data_raw.NOx, color='r', linewidth=0.75)
-sns1.plot(data_raw.index, data_raw.SOx, color='g', linewidth=0.75)
-sns1.plot(data_raw.index, data_raw.DUST, color='b', linewidth=0.75)
-sns1.plot(data_raw.index, data_raw.HCl, color='y', linewidth=0.75)
-sns1.plot(data_raw.index, data_raw.CO, color='m', linewidth=0.75)
+sns1.plot(data_fix.index, data_fix.NOx, color='r', linewidth=0.75)
+sns1.plot(data_fix.index, data_fix.SOx, color='g', linewidth=0.75)
+sns1.plot(data_fix.index, data_fix.DUST, color='b', linewidth=0.75)
+sns1.plot(data_fix.index, data_fix.HCl, color='y', linewidth=0.75)
+sns1.plot(data_fix.index, data_fix.CO, color='m', linewidth=0.75)
 plt.legend(['NOx', 'SOx', 'DUST', 'HCl', 'CO'], fontsize=18, loc='best')
-plt.xlabel('Date (2022/08/01~2022/10/31)', fontsize=18)
+plt.xlabel('Date', fontsize=18)
 plt.ylabel('TMS data', fontsize=18)
 plt.xticks(fontsize=18)
 plt.yticks(fontsize=18)
 
-# X_train = data_raw_train[['NOx', 'SOx', 'HCl', 'CO']]
-# X_test = data_raw_test[['NOx', 'SOx', 'HCl', 'CO']]
-X_train = data_raw_train.sort_index().iloc[:2741, :]
-y_train = data_raw_train[['NOx', 'SOx', 'HCl', 'CO']]
-print(X_train.shape)
-print(y_train.shape)
-X_test = data_raw_test['2022-10-01':'2022-10-21']
-print('data_raw.dtype: ', data_raw.dtypes)
-
 # 3. Build the several machine learning models
-# Build LR model
+# Build LR model (Time-Series)
 print("\n*** Build LinearRegression model ***")
 print(f'Checked sklearn version: {sklearn.__version__}')
-LR = LinearRegression()
-LR.fit(X_train, y_train)
-print('기울기 a:', LR.coef_)
-print('y절편 b:', LR.intercept_)
-relation_square = LR.score(X_train, y_train)
-print('결정 계수 r2: {:.2f}'.format(relation_square))
-LR_pred = LR.predict(X_test)
-print('y_pred: \n', LR_pred)
+LRTS = LinearRegression()
+LRTS.fit(X_train_ts, y_train_ts)
+print('LRTS.coef_: {}'.format(LRTS.coef_))
+print('LRTS.intercept_: {}'.format(LRTS.intercept_))
+print('Score of train datasets: {:.3f}'.format(LRTS.score(X_train_ts, y_train_ts)))
+# print('Score of test datasets: {:.3f}'.format(LRTS.score(X_test_ts, y_test_ts)))
+# LRTS_pred = LRTS.predict(X_test_ts)
+# print('LRTS_pred: \n', LRTS_pred)
+
+# Build LR model (Random)
+print("\n*** Build LinearRegression model ***")
+print(f'Checked sklearn version: {sklearn.__version__}')
+LRRND = LinearRegression()
+LRRND.fit(X_train_rnd, y_train_rnd)
+print('LRTS.coef_: {}'.format(LRRND.coef_))
+print('LRTS.intercept_: {}'.format(LRRND.intercept_))
+print('결정 계수 r2: {:.3f}'.format(LRRND.score(X_train_rnd, y_train_rnd)))
+LRRND_pred = LRRND.predict(X_test_rnd)
+print('LRRND_pred: \n', LRRND_pred)
 
 # 기울기 a: [[-0.00225756  0.02130299 -0.05156007 -0.00456313]]
 # y절편 b: [0.27532916]
@@ -121,10 +170,10 @@ print('y_pred: \n', LR_pred)
 print("\n*** Build RandomForestRegressor model ***")
 print(f'Checked sklearn version: {sklearn.__version__}')
 RFR = RandomForestRegressor(n_estimators=500, n_jobs=8, oob_score=True, random_state=0)
-RFR.fit(X_train, y_train)
+RFR.fit(X_train_ts, y_train_ts)
 importance = RFR.feature_importances_
 
-feature = X_train.columns
+feature = X_train_ts.columns
 importances = pd.DataFrame()
 importances['feature'] = feature
 importances['importances'] = importance
@@ -134,12 +183,28 @@ print(importances)
 
 plt.figure(figsize=(10,8))
 sns.barplot(x='importances', y='feature', data=importances)
-plt.title('Feature importance of the input variables on Random Forest', fontsize=18)
+plt.title('Feature importance of the input variables on Random Forest', fontsize=16)
+plt.xlabel('Importances', fontsize=16)
+plt.xticks(fontsize=16)
+plt.ylabel('Features', fontsize=16)
+plt.yticks(fontsize=16)
 plt.show()
 
 print('oob_prediction_:\n', RFR.oob_prediction_)
 print('oob_score_:\n', RFR.oob_score_)
-print('Train accuracy: ', RFR.score(X_train, y_train))
+print('Train accuracy: ', RFR.score(X_train_ts, y_train_ts))
+print('Test accuracy: ', RFR.score(X_train_ts, X_test_ts))
+
+RFR_pred = RFR.predict(X_test_ts)
+mesure_LR = np.exp(LRRND_pred)
+mesure_RFR = np.exp(RFR_pred)
+
+plt.yticks(fontname='Arial')
+plt.semilogy(X_te_ts.index, X_te_ts.NOx, label='Train data')
+plt.semilogy(X_te_ts.index, X_te_ts.NOx, label='Test data')
+plt.semilogy(X_te_ts.index, RFR_pred, label='Prediction of RFR')
+plt.semilogy(X_te_ts.index, LRRND_pred, label='Prediction of LR')
+plt.legend()
 
 # NOX: 0.31325047
 # SOX: 0.28792656
@@ -180,7 +245,7 @@ print('Train accuracy: ', RFR.score(X_train, y_train))
 
 # Build ANN model
 ANN = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5,2), random_state=1)
-ANN.fit(X_train, y_train)
+ANN.fit(X_train_ts, y_train_ts)
 # ANN_pred = ANN.predict(X_test)
 # print('ANN_pred: ', ANN_pred)
 print('ANN.coefs_: ', ANN.coefs_)
