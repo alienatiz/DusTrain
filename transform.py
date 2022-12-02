21fbdb419a131c098a35f8aca2bacd910142f34c
Byeongcheol Kim <kbc20000@gmail.com>
2022-12-02 17:39:34 +0900

    DusTrain: Improve the source codes for transposing
    * Fix the path datasets is in.
    * Add the codes drop the rows duplicated.
    
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- transform.py
+++ transform.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
-
-data_path = r'C:/Users/KBC/PycharmProjects/chemical/datasets'
-datasets = pd.read_csv(r'C:/Users/KBC/PycharmProjects/chemical/datasets/data_r2_fix.csv')
+data_path = r'D:/tms/datasets/'
+datasets = pd.read_csv(r'D:/tms/datasets/datasets_0801-1017.csv')
 print(datasets.head(5))
 
+datasets.drop_duplicates(inplace=True)
 fixed_datasets = datasets.transpose()
 print(fixed_datasets.head(5))
 
-fixed_datasets.to_csv(data_path + '\data_r2_fix_tp.csv', sep=',', na_rep='NaN', index=False, encoding='UTF-8-sig')
+fixed_datasets.to_csv(data_path + '/datasets_0801-1017_tp.csv', sep=',',
+                      na_rep='NaN', index=False, encoding='UTF-8-sig')
