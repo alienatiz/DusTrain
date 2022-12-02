4705f07f4d19c8d1f870182ddd46cab528263885
Byeongcheol Kim <kbc20000@gmail.com>
2022-12-02 18:59:32 +0900

    DusTrain: Improve the source codes for ts2rnd
    * Change property from time-series to random (ts2rnd)
    
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- ts2rnd.py
+++ ts2rnd.py
@@ -1,29 +1,31 @@
-import numpy
+import numpy as np
 import pandas as pd
 
 
-class ts2rnd:
+class Ts2rnd:
+    filename = ''
     datasets_path = ''
     input_df, output_df = pd.DataFrame, pd.DataFrame
     li1, li2 = [], []
     output = ''
 
-    def __init__ (self, filename):
-        pass
+    def __init__(self, filename, input_df):
+        self.input_df = input_df
+        self.filename = filename
 
-    def setFile(self, filename):
-        ts2rnd.datasets_path = r'D:/tms/datasets/'
-        ts2rnd.input_df = pd.read_csv(ts2rnd.datasets_path + str(filename))
-        return ts2rnd.input_df
+    def setFile(self, f_name):
+        datasets_path = r'D:/tms/datasets/'
+        filename = datasets_path + f_name + '.csv'
+        self.input_df = pd.read_csv(filename)
+        return Ts2rnd.input_df
 
     def dropData(self):
-        ts2rnd.input_df.drop_duplicates(inplace=True)
-        ts2rnd.input_df.drop(['mesure_dt', 'area_nm', 'stackCode', 'SOx', 'DUST', 'HCl', 'CO'], axis='columns',
-                             inplace=True)
-        return ts2rnd.input_df
+        self.input_df.drop_duplicates(inplace=True)
+        self.input_df.drop(['mesure_dt', 'area_nm', 'stackCode', 'SOx', 'DUST', 'HCl', 'CO'], axis='columns',
+                           inplace=True)
+        return self.input_df
 
     def saveCsv(self):
-        ts2rnd.output = 'ts2rnd.csv'
-        ts2rnd.datasets_path = r'D:/tms/datasets/ts2rnd/'
-        ts2rnd.input_df.to_csv("{0}{1}".format(ts2rnd.datasets_path, str(ts2rnd.output)))
-
+        self.output = 'ts2rnd.csv'
+        self.datasets_path = r'D:/tms/datasets/ts2rnd/'
+        self.input_df.to_csv("{0}{1}".format(Ts2rnd.datasets_path, str(Ts2rnd.output)))
